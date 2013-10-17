c1e6985e9b1522b200d168d0171c5f0b20db591a
DavidZeferino <david.freedows@gmai.com>
2013-10-17 00:07:47 +0000

    Sexto exercicios do project euler.

---
+++ 06.py
@@ -0,0 +1,6 @@
+soma_do_quadrado = sum([n**2 for n in range(1,100+1)])
+print 'Soma do quadrado : %i'%(soma_do_quadrado)
+soma = sum(range(1,100+1))
+print 'O quadrado da soma é %i:' %soma
+
+print 'A diferença dos primeiro 100 numeros natuaris e o quadrado da soma %i: '%(soma_do_quadrado-soma)
