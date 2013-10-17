e9793417d6adc208735cb7c7b9f493f43ef69216
DavidZeferino <david.freedows@gmai.com>
2013-10-17 01:03:24 +0000

    Modificacao de coding

--- 06.py
+++ 06.py
@@ -1,6 +1,10 @@
-soma_do_quadrado = sum([n**2 for n in range(1,100+1)])
-print 'Soma do quadrado : %i'%(soma_do_quadrado)
-soma = sum(range(1,100+1))
-print 'O quadrado da soma Ã© %i:' %soma
+# -*-coding: latin-1 -*-
+""" Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum."""
 
-print 'A diferenÃ§a dos primeiro 100 numeros natuaris e o quadrado da soma %i: '%(soma_do_quadrado-soma)
+
+soma_do_quadrado = sum([n**2 for n in range(1,100+1)])
+print 'A soma dos quadrados dos primeiros dez números naturais é: %i' %(soma_do_quadrado)
+soma = sum(range(1,100+1))
+print 'O quadrado da soma dos primeiros cem números naturais é: %i' %(soma)
+
+print 'A diferença dos primeiros 100 números natuaris e o quadrado da soma: %i ' %(soma_do_quadrado-soma)
