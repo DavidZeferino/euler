22fa7ebcd1cd2b4e288fbe05783ed270c303b292
david.freedows@gmail.com <david.freedows@gmail.com>
2013-11-05 17:16:18 -0200

    melhoria de leitura do código

--- 14.py
+++ 14.py
@@ -1,19 +1,23 @@
-def collatz(n, count = 1):
-	while n > 1:
-		count += 1
-		if n % 2 ==0:
-			n =  n/2
-		else:
-			n =  3*n + 1
-	return count
-
-
-max = [0,0]
-for i in range (1000000):
-	c = collatz(i)
-	if c > max[0]:
-		max[0] = c
-		max [1] = i
-print max[1]
-
-
+def contar_cadeia_collatz(numero):
+    tamanho_da_cadeia = 1
+    while numero > 1:
+        tamanho_da_cadeia += 1
+        if numero % 2 == 0:
+            numero /= 2
+        else:
+            numero =  3 * numero + 1
+    return tamanho_da_cadeia
+ 
+ 
+possivel_resposta = {
+    'numero' : 0,
+    'tamanho_da_cadeia' : 0
+}
+ 
+for numero_possivel in range (1000000):
+    tamanho_da_cadeia = contar_cadeia_collatz(numero_possivel)
+    if tamanho_da_cadeia > possivel_resposta['tamanho_da_cadeia']:
+        possivel_resposta['tamanho_da_cadeia'] = tamanho_da_cadeia
+        possivel_resposta['numero'] = numero_possivel
+ 
+print possivel_resposta['numero']
