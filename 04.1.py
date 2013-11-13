a1ea8b0e47542da3fb19d128affd1e7921c67d65
david.freedows@gmail.com <david.freedows@gmail.com>
2013-11-13 13:47:23 -0200

    refatorado

--- 04.1.py
+++ 04.1.py
@@ -1,11 +1,10 @@
 maior_palindromo = 0 
 for contagem_inversa_numero in range(999,0,-1):
-    for b in range(999,contagem_inversa_numero,-1):
-        print b
-        if contagem_inversa_numero * b < maior_palindromo:
+    for contagem_polidromo in range(999,contagem_inversa_numero,-1):
+        if contagem_inversa_numero * contagem_polidromo < maior_palindromo:
             break
-        palidromo = '%i'%(contagem_inversa_numero * b)
+        palidromo = '%i'%(contagem_inversa_numero * contagem_polidromo)
         if palidromo==palidromo[::-1]:
-            maior_palindromo = max(maior_palindromo,contagem_inversa_numero * b)
+            maior_palindromo = max(maior_palindromo,contagem_inversa_numero * contagem_polidromo)
             break
 print maior_palindromo
